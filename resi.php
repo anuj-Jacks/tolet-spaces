5b86400ccc493123dec374d1a0f7d00b1f2e2c3f
anuj kumar <anujanand2010@gmail.com>
2018-01-03 16:55:30 +0530

    3jan 5pm

--- resi.php
+++ resi.php
@@ -1,3 +1,41 @@
+<?php
+
+
+if(isset($_POST['search']))
+{
+	if($_POST['valueToSearch']==!"")
+	{
+    $valueToSearch = $_POST['valueToSearch'];
+    $query = "SELECT * FROM property WHERE Oname LIKE '%".$valueToSearch."%'";
+	$query = "SELECT * FROM property WHERE Locality LIKE '%".$valueToSearch."%'";
+	$query = "SELECT * FROM property WHERE bhk LIKE '%".$valueToSearch."%'";
+    $search_result = filterTable($query);
+	}
+	else {
+	 $query = "SELECT * FROM property WHERE ResType = 'family'";
+    $search_result = filterTable($query);
+}
+    
+}
+ elseif (isset($_POST['filter2'])) {
+    $query = "SELECT * FROM property WHERE ResType = 'bachelor'";
+    $search_result = filterTable($query);
+}
+else {
+	 $query = "SELECT * FROM property WHERE ResType = 'family'";
+    $search_result = filterTable($query);
+}
+
+// function to connect and execute the query
+function filterTable($query)
+{
+    $connect = mysqli_connect("localhost", "root", "", "tolet");
+    $filter_Result = mysqli_query($connect, $query);
+    return $filter_Result;
+}
+
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -24,6 +62,50 @@
 
 <body>
 <div>
+ <form action="resi.php" method="post">
+		 
+			<input type="submit" name="filter1" value="Family">
+		    <input type="submit" name="filter2" value="Bachelor"><br><br>
+			
+			
+		
+            <input type="text" name="valueToSearch" placeholder="Value To Search">
+			<select name="valueToSearch">
+				<option  value="Gomtinagar">Gomtinagar</option>
+				<option  value="GomtinagarEX">Gomtinagar Extension</option>
+				<option  value="hazratganj">Hazaratganj</option>
+            </select>
+			<select name="valueToSearch">
+				<option  value="2">2 BHK</option>
+				<option  value="3">3 BHK</option>
+				<option  value="4">4 BHK</option>
+            </select>
+			<br><br>
+            <input type="submit" name="search" value="Filter"><br><br>
+            
+            <table>
+                <tr>
+                    <th>Address</th>
+                    <th>Area</th>
+                    <th>Budget</th>
+                    <th>Furnishing</th>
+					<th>bhk</th>
+					<th>ResType</th>
+                </tr>
+
+      <!-- populate table from mysql database -->
+                <?php while($row = mysqli_fetch_array($search_result)):?>
+                <tr>
+                    <td><?php echo " " .$row['Housenum']. " " .$row['Locality']. " " .$row['City']. " " .$row['State']. " " .$row['Country']." <br> " .$row['pincode']. " ";?></td>
+                    <td><?php echo $row['Area'];?></td>
+                    <td><?php echo $row['Budget'];?></td>
+                    <td><?php echo $row['Furnishing'];?></td>
+					<td><?php echo $row['bhk'];?></td>
+					<td><?php echo $row['ResType'];?></td>
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
 </div>
 
 
--- test4.php
+++ test4.php
@@ -1,16 +1,27 @@
 <?php
 
+
 if(isset($_POST['search']))
 {
+	if($_POST['valueToSearch']==!"")
+	{
     $valueToSearch = $_POST['valueToSearch'];
-    // search in all table columns
-    // using concat mysql function
     $query = "SELECT * FROM property WHERE Oname LIKE '%".$valueToSearch."%'";
+	 $query = "SELECT * FROM property WHERE Locality LIKE '%".$valueToSearch."%'";
     $search_result = filterTable($query);
+	}
+	else {
+	 $query = "SELECT * FROM property WHERE ResType = 'family'";
+    $search_result = filterTable($query);
+}
     
 }
- else {
-    $query = "SELECT * FROM property";
+ elseif (isset($_POST['filter2'])) {
+    $query = "SELECT * FROM property WHERE ResType = 'bachelor'";
+    $search_result = filterTable($query);
+}
+else {
+	 $query = "SELECT * FROM property WHERE ResType = 'family'";
     $search_result = filterTable($query);
 }
 
@@ -38,24 +49,39 @@ function filterTable($query)
     <body>
         
         <form action="test4.php" method="post">
-            <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
+		 
+			<input type="submit" name="filter1" value="Family">
+		    <input type="submit" name="filter2" value="Bachelor"><br><br>
+		
+            <input type="text" name="valueToSearch" placeholder="Value To Search">
+						<select name="valueToSearch">
+				<option   value="Gomtinagar">Gomtinagar</option>
+				<option   value="GomtinagarEX">Gomtinagar extension</option>
+				<option   value="hazratganj">Hazaratganj</option>
+  
+</select>
+			<br><br>
             <input type="submit" name="search" value="Filter"><br><br>
             
             <table>
                 <tr>
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Age</th>
+                    <th>Address</th>
+                    <th>Area</th>
+                    <th>Budget</th>
+                    <th>Furnishing</th>
+					<th>bhk</th>
+					<th>ResType</th>
                 </tr>
 
       <!-- populate table from mysql database -->
                 <?php while($row = mysqli_fetch_array($search_result)):?>
                 <tr>
-                    <td><?php echo $row['id'];?></td>
-                    <td><?php echo $row['Oname'];?></td>
-                    <td><?php echo $row['Ocontact'];?></td>
-                    <td><?php echo $row['Oemail'];?></td>
+                    <td><?php echo " " .$row['Housenum']. " " .$row['Locality']. " " .$row['City']. " " .$row['State']. " " .$row['Country']." <br> " .$row['pincode']. " ";?></td>
+                    <td><?php echo $row['Area'];?></td>
+                    <td><?php echo $row['Budget'];?></td>
+                    <td><?php echo $row['Furnishing'];?></td>
+					<td><?php echo $row['bhk'];?></td>
+					<td><?php echo $row['ResType'];?></td>
                 </tr>
                 <?php endwhile;?>
             </table>
